622d2aa0826821d5131d0172bee017a18d563881
Thibault Dolley <thibault.dolley@epitech.eu>
2022-10-16 13:03:54 +0200

    Added details scrapper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,64 @@
-import { parse } from "node-html-parser"
+import { parse, HTMLElement } from "node-html-parser"
 import { readFileSync, writeFileSync } from "fs"
 import axios from "axios"
+
+function findSpecialInfoNode(root: HTMLElement, type: string): string | undefined {
+    const typeNode = root.querySelector(`dl dt a[title=${type}]`)
+    const parentNode = typeNode?.parentNode;
+    const targetedNode = parentNode?.nextElementSibling
+    const specialInfo = (targetedNode?.firstChild as unknown as HTMLElement)?.getAttribute('title')
+    return specialInfo;
+}
+
+class SkillsDetails {
+    public energyCost!: string | undefined;
+    public cooldown!: string | undefined;
+    public castingTime!: string | undefined;
+    public adrenalineCost!: string | undefined;
+    public upkeep!: string | undefined;
+    public sacrifice!: string | undefined;
+    public overcast!: string | undefined;
+    public skillImage!: string | undefined;
+    public attribute!: string | undefined;
+    public campaign!: string | undefined;
+    public profession!: string | undefined;
+    public type!: string | undefined;
+}
+
+async function getAllDataFromSkill(url: string): Promise<SkillsDetails> {
+    const detailsHtml = (await axios.get(url)).data
+    const detailsRoot = parse(detailsHtml)
+    findSpecialInfoNode(detailsRoot, "Profession")
+
+    const energyCost = (detailsRoot.querySelector(".skill-stats  ul li a[title=Energy]")?.parentNode)?.rawText
+    const cooldown = (detailsRoot.querySelector(".skill-stats  ul li a[title=Recharge]")?.parentNode)?.rawText
+    const castingTime = (detailsRoot.querySelector(".skill-stats  ul li a[title=Activation]")?.parentNode)?.rawText
+    const adrenalineCost = (detailsRoot.querySelector(".skill-stats  ul li a[title=Adrenaline]")?.parentNode)?.rawText
+    const upkeep = (detailsRoot.querySelector(".skill-stats  ul li a[title=Upkeep]")?.parentNode)?.rawText
+    const sacrifice = (detailsRoot.querySelector(".skill-stats  ul li a[title=Sacrifice]")?.parentNode)?.rawText
+    const overcast = (detailsRoot.querySelector(".skill-stats  ul li a[title=Overcast]")?.parentNode)?.rawText
+
+    const skillImage = detailsRoot.querySelector("div.skill-box  div.skill-image a img")?.getAttribute("src")
+    const attribute = findSpecialInfoNode(detailsRoot, ("Attribute"))
+    const campaign = findSpecialInfoNode(detailsRoot, ("Campaign"))
+    const profession = findSpecialInfoNode(detailsRoot, ("Profession"))
+    const type = findSpecialInfoNode(detailsRoot, ('"Skill type"'))
+
+    return {
+        energyCost,
+        cooldown,
+        castingTime,
+        adrenalineCost,
+        upkeep,
+        sacrifice,
+        overcast,
+        skillImage,
+        attribute,
+        campaign,
+        profession,
+        type,
+    };
+}
 
 async function exec() {
     // The html is an edited version of the skills Id Hosted at GWW, with an id inserted in the desired element (start of the table body)
@@ -25,8 +83,8 @@ async function exec() {
     writeFileSync("./skills.json", JSON.stringify(skills, null, 2))
 
 
-    const detailsPage = await axios.get(skills["Cleave"].detailsPage)
-    console.log(detailsPage)
+
+    console.log(await getAllDataFromSkill(skills["Dragon's Stomp"].detailsPage))
 
 }
 
